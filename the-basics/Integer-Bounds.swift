2f4600e707bea779838581a9a28c5d87df61eed3
Roy Vanegas <roy@thecodeeducators.com>
2024-01-30 19:20:35 -0500

    Add Integer-Bounds.swift

---
+++ the-basics/Integer-Bounds.swift
@@ -0,0 +1,8 @@
+print("The minimum unsigned 8-bit integer is \(UInt8.min)")
+print("The maximum unsigned 8-bit integer is \(UInt8.max)", terminator: "\n\n")
+
+print("The minimum signed integer is \(Int.min)")
+print("The maximum signed integer is \(Int.max)", terminator: "\n\n")
+
+print("The minimum unsigned integer is \(UInt.min)")
+print("The maximum unsigned integer is \(UInt.max)")
