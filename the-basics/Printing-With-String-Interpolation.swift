6eb5a8981138dbcaea07774b979f0365670e018d
Roy Vanegas <roy@thecodeeducators.com>
2024-01-30 19:20:23 -0500

    add Printing-With-String-Interpolation.swift

---
+++ the-basics/Printing-With-String-Interpolation.swift
@@ -0,0 +1,9 @@
+// Unicode characters are fine as variable/constant names
+let π = 3.14159
+let 你好 = "界" // “hello” = “world”
+let 🐶🐮 = "dog and cow"
+
+// Note how \(VAR_OR_CONSTANT_NAME) is used for string interpolation
+print("π is equal to \(π)")
+print("你好, \(你好)")
+print("🐶🐮 are a \(🐶🐮)")
