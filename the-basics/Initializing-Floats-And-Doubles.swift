bc2d7d4bbc11d1a9579ccd163cbb8ebfedabfb36
Roy Vanegas <roy@thecodeeducators.com>
2024-02-04 06:58:05 -0500

    Expand example in Initializing-Floats-And-Doubles.swift

--- the-basics/Initializing-Floats-And-Doubles.swift
+++ the-basics/Initializing-Floats-And-Doubles.swift
@@ -1,8 +1,11 @@
 let PI = 3.14159
+let ANOTHER_PI = 3 + 0.14159
 var fraction: Float = 3.2
+
 
 print(PI)
 print(fraction)
 
 print("Note how, without an explicit declaration, a floating-point number defaults to a “float”: \(type(of: PI))")
 print("To declare a `Float`, I must be explicit with a type annotation: \(type(of: fraction))")
+print("If you combine integer and floating-point literals in an expression, a type of Double will be inferred from the context. Thus, ANOTHER_PI is a \(type(of: ANOTHER_PI))")
