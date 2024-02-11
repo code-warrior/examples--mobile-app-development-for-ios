f939bdd48835e3b4056d095e1769e9e280060838
Roy Vanegas <roy@thecodeeducators.com>
2024-02-11 10:40:19 -0500

    Add comparison code snippet to Booleans.swift

--- the-basics/Booleans.swift
+++ the-basics/Booleans.swift
@@ -1,5 +1,6 @@
 let cilantroTastesDisgusting = true
 let parsleyIsDelicious = false
+let TEN = 10
 
 /*
   Note that the following two constants were inferred to be Booleans from their
@@ -18,4 +19,13 @@ if parsleyIsDelicious {
   print("Awesome! I love parsley on my rice, especially")
 } else {
   print("Like cilantro, parlsey tastes like soap and is disgusting")
+}
+
+/*
+  In the following example, TEN the constants is compared with the integer 10.
+  The comparison evaluates to a Boolean, in this case, “true.”
+*/
+
+if TEN == 10 {
+  print("TEN equal 10")
 }
