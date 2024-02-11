d22f4c9e7ae7effcab64421c75babb5cffaa42bf
Roy Vanegas <roy@thecodeeducators.com>
2024-02-11 13:42:39 -0500

    Update optionals example

--- the-basics/Optionals.swift
+++ the-basics/Optionals.swift
@@ -4,14 +4,30 @@ let number = Int(numberAsAString)
 print(type(of: numberAsAString))
 print(type(of: number))
 
+// Both of the following will throw a warning
 print(numberAsAString)
 print(number)
 
 var serverResponseCode: Int? = 404
 
+// Use force unwrapping (the bang character at the end of the variable) to
+// print the value of the optional variable serverResponseCode
+if serverResponseCode != nil {
+  print("The server’s response code is \(serverResponseCode!)")
+}
+
+// Without forced unwrapping, you’ll get a warning from Swift
 print("The value of serverResponseCode is \(serverResponseCode)")
 
 serverResponseCode = nil
+
+if serverResponseCode == nil {
+  serverResponseCode = 200
+}
+
+if serverResponseCode == 200 {
+  print("Response from server is OK: \(serverResponseCode!)")
+}
 
 print(type(of: serverResponseCode))
 print("The value of serverResponseCode is \(serverResponseCode)")
@@ -20,3 +36,7 @@ print("The value of serverResponseCode is \(serverResponseCode)")
 var otherServerResponseCode: String?
 
 print("The value of otherServerResponseCode is \(otherServerResponseCode)")
+
+/*
+  IMPORTANT NOTE: “Always make sure that an optional contains a non-nil value before using ! to force-unwrap its value.” Otherwise, you’ll get a runtime error.
+*/
