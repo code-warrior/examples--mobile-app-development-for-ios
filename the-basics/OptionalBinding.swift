c06ecfbf65899e97f4438be43e36b9e6fcf7a4bc
Roy Vanegas <roy@thecodeeducators.com>
2024-02-11 19:27:15 -0500

    Add OptionalBinding.swift

---
+++ the-basics/OptionalBinding.swift
@@ -0,0 +1,22 @@
+let serverResponseCode = "404"
+
+/*
+  If the optional `Int` returned by `Int(serverResponseCode)` contains a value,
+  set a new constant called `serverResponse` to the value contained in the
+  optional. Moreover, if the conversion is successful, `serverResponse` becomes
+  available for use in the if section.
+*/
+if let serverResponse = Int(serverResponseCode) {
+  print(serverResponse)
+}
+
+let response = Int(serverResponseCode)
+
+if let response = response {
+  print(response)
+}
+
+// Lines 15–17 are equivalent to
+if let response {
+  print(response)
+}
